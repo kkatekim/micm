aeb69c27d0d6887d637cd572f0acdadf98098e6f
kkatekim <kkatekim@outlook.com>
2021-05-27 17:29:59 -0700

    deleted directories

--- gene_burden_project/find_subset.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-def find_subset(df, column_name, condition):
-    '''finds the rows in the column with the specified value'''
-    return df[df[column_name] == condition]
-
